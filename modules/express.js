1deb57126e53606093552840b48e6851b6f1b943
Igor Luna <igor.lunarx@gmail.com>
2023-12-27 15:16:58 -0300

    [FEAT] Adding patch and delete methods

--- modules/express.js
+++ modules/express.js
@@ -32,6 +32,36 @@ app.post("/users", async (req, res) => {
   }
 });
 
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await UserModel.findById(id);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await UserModel.findByIdAndUpdate(id, req.body);
+    res.status(200).json(req.body);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await UserModel.findByIdAndDelete(id);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando express na porta ${port}`));
